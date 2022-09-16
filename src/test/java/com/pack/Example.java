89c9e56a92f1dd1824cf315d64c7cf0c9c9a3485
Santhosh5717 <Santhosh.ntp@gmail.com>
2022-09-16 16:34:17 +0530

    greens update

--- src/test/java/com/pack/Example.java
+++ src/test/java/com/pack/Example.java
@@ -4,21 +4,31 @@ public class Example {
 	private void method() {
 		System.out.println("1");
 	}
+
 	private void method2() {
 		System.out.println("2");
 	}
+
 	private void method3() {
 		System.out.println("3");
 	}
+
 	private void method4() {
 		System.out.println("4");
 	}
+
 	private void method5() {
 		System.out.println("5");
 	}
+
 	private void method6() {
 		System.out.println("6");
 	}
+
+	private void method7() {
+		System.out.println("7");
+	}
+
 	public static void main(String[] args) {
 		Example e = new Example();
 		e.method();
@@ -27,5 +37,6 @@ public class Example {
 		e.method4();
 		e.method5();
 		e.method6();
+		e.method7();
 	}
 }
