b2cafa2630e63225b986c2042895ffcdc812f30b
Santhosh5717 <Santhosh.ntp@gmail.com>
2022-09-16 16:03:53 +0530

    Master Code

---
+++ src/test/java/com/pack/Example.java
@@ -0,0 +1,23 @@
+package com.pack;
+
+public class Example {
+	private void method() {
+		System.out.println("1");
+	}
+	private void method2() {
+		System.out.println("2");
+	}
+	private void method3() {
+		System.out.println("3");
+	}
+	private void method4() {
+		System.out.println("4");
+	}
+	public static void main(String[] args) {
+		Example e = new Example();
+		e.method();
+		e.method2();
+		e.method3();
+		e.method4();
+	}
+}
